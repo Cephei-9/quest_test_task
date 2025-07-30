3da3f7ece91622b58f8914ba78ab709e6e88e5ed
Cephei <mzailogin@gmail.com>
2025-07-30 18:03:55 +0300

    wrote final scene of story

---
+++ Quest(Unity Projcet)/Assets/Scripts/Locations/HideLocationCommand.cs
@@ -0,0 +1,14 @@
+﻿using Locations;
+using Naninovel;
+
+[CommandAlias("hideLocation")]
+public class HideLocationCommand : Command
+{
+    public override UniTask ExecuteAsync(AsyncToken asyncToken = default)
+    {
+        LocationService locationService = Engine.GetService<LocationService>();
+        locationService.HideLocation();
+            
+        return UniTask.CompletedTask;
+    }
+}
